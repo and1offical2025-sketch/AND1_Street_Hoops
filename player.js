716752fc184c5d74fa75c090410b99084325cdba
and1offical2025-sketch <and1offical2025@gmail.com>
2025-10-22 18:54:01 -0400

    Add files via upload

---
+++ player.js
@@ -0,0 +1,8 @@
+let player = {
+  build: "Sharpshooter",
+  attributes: {
+    threePoint: 85, layup: 70, dribbling: 75,
+    speed: 80, agility: 75, vertical: 70, stamina: 90
+  },
+  rep: 0, badges: []
+};
